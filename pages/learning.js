9c42c48ae793b7e745147e53e93f7d8a43e8b7bb
sun1723 <sunpeiting07@gmail.com>
2020-09-21 12:41:39 +0930

    add header

--- pages/learning.js
+++ pages/learning.js
@@ -1,8 +1,12 @@
+import styles from '../styles/Learning.module.css'
 const Learning = () =>(
     <>
-        {/*head*/}
-        <div>
-
+        {/*header*/}
+        <div className={styles.header}>
+            <div class="header-section">
+                <h1>Scroll down to see the parallax effect</h1>
+                <h2>Background landcape scrolls with its own depth </h2>
+            </div>
         </div>
         {/*side*/}
 
